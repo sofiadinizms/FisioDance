167b5111a9fcace14244fac42b8a4e459d6035d2
hibianca <103222620+hibianca@users.noreply.github.com>
2023-04-03 10:16:55 -0300

    feat(help-view): help view faltando cores e info completas

--- FisioDance/HelpView.swift
+++ FisioDance/HelpView.swift
@@ -8,23 +8,43 @@
 import SwiftUI
 
 struct HelpView: View {
+    
+    @State private var items = HelpItems.exampleData
+    @State private var selection: HelpItems?
+    
     var body: some View {
-        NavigationStack{
-            VStack{
-                
-                NavigationLink(destination: ContentView()){
-                    Text("Voltar para home")
-                        .font(.custom("Jura", size: 30))
-                }
-                
+        NavigationSplitView{
+            List(items, selection: $selection) { item in
+                Text(item.name).tag(item)
             }
+        } detail: {
+            Text(selection?.details ?? "Escolha ao lado")
+                .navigationTitle(selection?.name ?? "")
+            
         }
         .navigationTitle("Ajuda")
+        .navigationSplitViewStyle(.balanced)
     }
 }
 
 struct HelpView_Previews: PreviewProvider {
     static var previews: some View {
         HelpView()
+    }
+}
+
+struct HelpItems: Identifiable, Hashable {
+    let id = UUID()
+    let name: String
+    let details: String
+}
+
+extension HelpItems {
+    static var exampleData: [HelpItems] {
+        [
+            .init(name: "Título 1", details: "Infos da página de Título 1"),
+            .init(name: "Título 2", details: "Infos da página de Título 2"),
+            .init(name: "Título 3", details: "Infos da página de Título 3"),
+        ]
     }
 }
